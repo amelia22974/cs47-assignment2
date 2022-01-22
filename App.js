f47ab4982ac4b5e476264ce36f63489483cd1d99
amelia22974 <amelia.a.woodward@gmail.com>
2022-01-22 01:47:03 -0800

    Add basic features pre spacing editing

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import AppLoading from 'expo-app-loading';
-import { StyleSheet, Text, View, StatusBar, Image, Button, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Image, Button, ImageBackground, Platform} from 'react-native';
 import { useFonts } from 'expo-font';
 import { Themes } from './assets/Themes';
 import themes from './assets/Themes/themes';
@@ -23,32 +23,80 @@ export default function App() {
     );
   }
 
+  const LabeledIcon = (props) => {
+    return(
+      <View style={styles.toolbarIcons}>
+          <Image style={styles.standardIcon} source={require("./assets/Icons/discover_light.png")}></Image>
+          <Text style={styles.iconText}> Discover</Text>
+      </View>
+    )
+  }
+
   const AudioBox = (props) => {
     return(
-      <View style={styles.audioBox}>
-      <Text style={styles.audioText}>My hottest take</Text>
-        <Image style={styles.standardIcon} source={require("./assets/Icons/player_light.png")}></Image>
-        <Image style={styles.audioIcon} source={require("./assets/Icons/audio_waveform_light.png")}></Image>
+      <View style={styles.audioSection}>
+        <Text style={styles.audioText}>My hottest take</Text>
+        <View style={styles.audioBox}>
+            <View style={styles.audioChild}>
+              <Image style={styles.standardIcon} source={require("./assets/Icons/player_light.png")}></Image>
+            </View>
+            <View style={styles.audioChild}>
+              <Image style={styles.audioIcon} source={require("./assets/Icons/audio_waveform_light.png")}></Image>
+            </View>
+        </View>
       </View>
     );
+  }
+
+  const BottomToolbar = (props) => {
+    return(
+      <View style={styles.bottomToolbar}>
+        <View style={styles.toolbarIcons}>
+          <Image style={styles.standardIcon} source={require("./assets/Icons/discover_light.png")}></Image>
+        </View>
+        <View style={styles.toolbarIcons}>
+          <Image style={styles.standardIcon} source={require("./assets/Icons/heart_light.png")}></Image>
+        </View>
+        <View style={styles.toolbarIcons}>
+          <Image style={styles.standardIcon} source={require("./assets/Icons/messages_light.png")}></Image>
+        </View>
+      </View>
+    )
+  }
+
+  const TopToolbar = (props) => {
+    return(<View style={styles.TopToolbar}>
+      <View style={styles.toolbarIcons}>
+          <Image style={styles.standardIcon} source={require("./assets/Icons/menu_light.png")}></Image>
+        </View>
+        <View style={styles.toolbarIcons}>
+          <Text style={styles.titleText}>ensom</Text>
+        </View>
+      <View style={styles.toolbarIcons}>
+          <Image style={styles.standardIcon} source={require("./assets/Icons/sun.png")}></Image>
+      </View>
+
+    </View>);
   }
   const MainCard = (props) => {
 
     return(
       <View>
-        <ProfilePic />
-        <AudioBox />
+        <TopToolbar />
+        <View style={styles.cardPiece}>
+          <ProfilePic />
+        </View>
+        <View styles={styles.cardPiece}>
+          <AudioBox />
+        </View>
       </View>
-      
     );
-
-    
-
   }
   
   return (
       <View style={styles.container}>
           <MainCard />
+          <BottomToolbar />
       </View>
     );
   }
@@ -57,16 +105,15 @@ export default function App() {
     const styles = StyleSheet.create({
       container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: themes.light.bg,
         alignItems: 'center',
         justifyContent: 'center',
       },
       card: {
-        width: 350,
-        height: 375,
+        width: 300,
+        height: 350,
         display: "flex",
         flexDirection: "row",
-  
       },
       profileText: {
         fontSize: 42,
@@ -75,37 +122,82 @@ export default function App() {
         textAlign: "left", 
         textAlignVertical: "bottom",
         paddingLeft: '2%'
-
       },
-      audioBox: {
+      audioSection: {
         width: 350,
         height: 150,
         borderRadius: 15,
-        backgroundColor: "gray",
+        backgroundColor: themes.light.bgSecondary,
+        shadowColor: themes.light.shadowColor,
+        shadowOffset: themes.light.shadows.shadowOffset,
+        shadowOpacity: themes.light.shadows.shadowOpacity,
+        shadowRadius: themes.light.shadows.shadowRadius,  
+
+      },
+      audioBox: {
+        display: "flex",
+        flexDirection: "row",
         padding:'3%',
-        shadowColor: Themes.light.shadows.shadowColor,
-        shadowOffset: Themes.light.shadows.shadowOffset,
-        shadowOpacity: Themes.light.shadowOpacity,
-        shadowRadius: Themes.light.shadowRadius
+      },
+      audioChild: {
+        display:"flex",
+        alignItems: "center", 
+        padding: '1%',
       },
       audioText: {
         fontFamily: "Sydney",
         color: Themes.light.text,
         fontSize: 30,
-        padding:'5%'
+        paddingTop:'5%',
+        paddingLeft: '5%'
       },
       standardIcon: {
-          width: 60,
-          height: 60
+          width: 45,
+          height: 45
       },
       audioIcon: {
-        width: 350,
-        height: 50
+        width: 250,
+        height:40
       },
       locationText: {
         fontFamily: "Sydney",
         color: Themes.light.textSecondary,
         textAlignVertical: "bottom",
         paddingLeft: '20%'
+      },
+      iconText: {
+        fontFamily: "Sydney",
+        color: Themes.light.textSecondary,
+        textAlignVertical: "bottom",
+        paddingLeft: '20%'
+      },
+      cardPiece:{
+        padding: '5%',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      },
+      toolbarIcons: {
+        marginTop: "auto",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between"
+      },
+      bottomToolbar:{
+        backgroundColor: "black",
+        position: 'absolute',
+        bottom: 0,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+      },
+      TopToolbar: {
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+      },
+      titleText:{
+        fontFamily: "Sydney-Bold",
+        fontSize: 42
       }
   });
