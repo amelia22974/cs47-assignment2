fce770c143395f9f848da1b8e0efe2ac527c6f8d
amelia22974 <amelia.a.woodward@gmail.com>
2022-01-22 00:06:46 -0800

    Add initial middle elements

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import AppLoading from 'expo-app-loading';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Image, Button, ImageBackground } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Themes } from './assets/Themes';
+import themes from './assets/Themes/themes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -13,31 +14,98 @@ export default function App() {
   StatusBar.setBarStyle(Themes.light.statusBar);
   /* ^Don't mind/edit this one either unless you decide to do the dark theme one, in that case, you will have to change it accordingly*/
 
-  /* insert your code here */
+  const ProfilePic = (props) => {
+    return(
+      <ImageBackground  style={styles.card} imageStyle={{ borderRadius: 10}} resizeMode="cover" source={require("./assets/Profiles/mtl.jpg")}>
+        <Text style={styles.profileText}>MTL</Text>
+        <Text style={styles.locationText}>2 miles away</Text>
+      </ImageBackground>
+    );
+  }
 
+  const AudioBox = (props) => {
+    return(
+      <View style={styles.audioBox}>
+      <Text style={styles.audioText}>My hottest take</Text>
+        <Image style={styles.standardIcon} source={require("./assets/Icons/player_light.png")}></Image>
+        <Image style={styles.audioIcon} source={require("./assets/Icons/audio_waveform_light.png")}></Image>
+      </View>
+    );
+  }
+  const MainCard = (props) => {
+
+    return(
+      <View>
+        <ProfilePic />
+        <AudioBox />
+      </View>
+      
+    );
+
+    
+
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          fontFamily: 'Sydney', // test to see if the font is loaded, feel free to remove this
-        }}>
-        Open up App.js to start working on your app!
-      </Text>
-      <Text
-        style={{
-          fontFamily: 'Sydney-Bold', // test to see if the font is loaded, feel free to remove this
-        }}>
-        ~Good luck~
-      </Text>
-    </View>
-  );
-}
+      <View style={styles.container}>
+          <MainCard />
+      </View>
+    );
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      card: {
+        width: 350,
+        height: 375,
+        display: "flex",
+        flexDirection: "row",
+  
+      },
+      profileText: {
+        fontSize: 42,
+        fontFamily: "Sydney",
+        color: Themes.light.textSecondary,
+        textAlign: "left", 
+        textAlignVertical: "bottom",
+        paddingLeft: '2%'
+
+      },
+      audioBox: {
+        width: 350,
+        height: 150,
+        borderRadius: 15,
+        backgroundColor: "gray",
+        padding:'3%',
+        shadowColor: Themes.light.shadows.shadowColor,
+        shadowOffset: Themes.light.shadows.shadowOffset,
+        shadowOpacity: Themes.light.shadowOpacity,
+        shadowRadius: Themes.light.shadowRadius
+      },
+      audioText: {
+        fontFamily: "Sydney",
+        color: Themes.light.text,
+        fontSize: 30,
+        padding:'5%'
+      },
+      standardIcon: {
+          width: 60,
+          height: 60
+      },
+      audioIcon: {
+        width: 350,
+        height: 50
+      },
+      locationText: {
+        fontFamily: "Sydney",
+        color: Themes.light.textSecondary,
+        textAlignVertical: "bottom",
+        paddingLeft: '20%'
+      }
+  });
